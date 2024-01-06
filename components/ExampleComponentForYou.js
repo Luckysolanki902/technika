e9aee3393d2415dfe9c3d1361a2a8726743d7e6a
Lucky <luckysolanki902@gmail.com>
2024-01-06 21:07:57 +0530

    made initial template

---
+++ components/ExampleComponentForYou.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './compstyles/examplecomp.module.css'
+
+const ExampleComponentForYou = () => {
+  return (
+    <div className={styles.mainDiv}>Example Component For You</div>
+  )
+}
+
+export default ExampleComponentForYou;
