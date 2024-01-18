3ccc3a9f2232384ed84f49f415358a744929288f
Sahil Shakoor <98870741+sahilshakoor786@users.noreply.github.com>
2024-01-19 00:57:14 +0530

    Add files via upload

---
+++ components/Navbar.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react';
+import styles from './compstyles/sidebar.module.css';
+import Image from 'next/image';
+import Menu from './Menu';
+import { useRouter } from 'next/router';
+import { width } from '@mui/system';
+import { Link } from '@mui/material';
+import Sidebar from '@/components/Sidebar'
+
+const Navbar = () => {
+  const router = useRouter()
+
+
+  const img = '/images/logo.png'
+ 
+  return (
+    <div  style={{width:"100%",color:"white",height:"100%",fontStyle:"italic",fontSize:"1.6rem",fontFamily:"sans-serif",display:"flex",justifyContent:"space-around",alignItems:"center"}}>
+   
+  
+  <div style={{padding:"5px",paddingRight:"10px"}}>
+  <Link href={"/events"} style={{outline:"none !important",textDecoration:"none",color:"white"}}>
+  <img src='https://drive.google.com/thumbnail?id=1kv88VtTDbPkW5iciCs33hTJVKQ_9LJaI' height={"40px"}/>
+    </Link>
+    
+    
+    </div>
+  <div style={{padding:"5px",paddingRight:"10px"}} >
+    <Link href={"/team"} style={{outline:"none !important",textDecoration:"none",color:"white"}}>
+    <img src='https://drive.google.com/thumbnail?id=1hGzk6993fPsu6YB0qjTzndf2ZGKoTFUn' height={"40px"}/>
+    </Link>
+    
+    </div>
+  <div style={{padding:"5px",paddingRight:"10px"}}>
+  <Link href={"/about"} style={{outline:"none !important",textDecoration:"none",color:"white"}}>
+  <img loading="eager" src='https://drive.google.com/thumbnail?id=1J4szZfqqDzVGvUDASbMTy05R0sHS24Nt' height={"40px"}/>
+    
+    </Link>
+    
+  </div>
+  <div style={{padding:"5px",paddingRight:"10px"}}>
+  <Link href={"/sponsors"} style={{outline:"none !important",textDecoration:"none",color:"white"}}>
+  <img src="https://drive.google.com/thumbnail?id=108j70VZnWLgngkGHdMA5vAnfuxREVp-z" height={"40px"}/>
+    </Link>
+    
+  </div>
+  <div style={{padding:"5px",paddingRight:"10px"}}>
+  <Link href={"/merchandise"} style={{outline:"none !important",textDecoration:"none",color:"white"}}>
+  <img src='https://drive.google.com/thumbnail?id=1ZDGceR_ef_8uXo5Pn4db0KxmHRvMIQK5' height={"40px"}/>
+    </Link>
+    
+  </div>
+<Sidebar/>
+    </div>
+  );
+};
+
+export default Navbar;
