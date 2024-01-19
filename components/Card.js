7e8656e3c35b459dca7adf0a717727c673b0e4b0
Lucky <luckysolanki902@gmail.com>
2024-01-19 22:20:25 +0530

    updated someones dp

--- components/Card.js
+++ components/Card.js
@@ -3,21 +3,24 @@ import style from "./compstyles/card.module.css";
 import Image from "next/image";
 
 const Card = ({ name, position, updatedImageUrl, number }) => {
-console.log(position)
+  const isLongName = name.length > 14;
+  const isTooLongName = name.length > 16;
   return (
     <div className={style.pro}>
       <div className={style.in}>  
         <div className={style.proin}>
           <Image
             src={`/local_images/imagestsc/${updatedImageUrl}`}
-            height={500}
-            width={500}
+            height={960}
+            width={1280}
+            quality={70}
+            // objectFit="contain"
             alt={'member'}
             className={style.proimg}
           />
           <div className={style.imgOver}></div>
         </div>
-        <div className={style.protxt}>{name}</div>
+        <div className={`${style.protxt} ${isLongName ? style.longName : ''} ${isTooLongName ? style.tooLongName : ''}`}>{name}</div>
         <div className={style.pos}>{position}</div>
       </div>
     </div>
