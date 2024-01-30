5a64ce8c0e16ff5f60495766112d6bdea9520233
Sumit Singh <124269664+Rusty-98@users.noreply.github.com>
2024-01-30 20:11:54 +0530

    Update Tshirtmodel.js

--- components/Tshirtmodel.js
+++ components/Tshirtmodel.js
@@ -8,7 +8,6 @@ const Tshirtmodel = () => {
     <div style={{display:'flex', justifyContent:'center', width:'100vw', height:'auto', flexDirection:"column", alignItems:'center'}} >
         <div className={styles.top}>Merch</div>
         <Mod />
-        // <Image data-cursor-color='rgba(0, 0, 0, 0.1)' data-cursor-size='300px' src={'/images/tshirtdummy.png'} width={2968} height={2808} alt='tshrit' style={{width:'50vw', height:'auto'}}></Image>
         <div style={{color:'#7f7f7f', textAlign:'center', fontSize:'2rem', fontFamily:'Jost', marginBottom:'3rem'}}>...COMING SOON...</div>
     </div>
   )
