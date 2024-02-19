4da7c509b29bb24d3c4668645041c4f6b0ee7eda
Lucky <luckysolanki902@gmail.com>
2024-02-19 20:16:24 +0530

    added nodemailer

--- pages/api/buymerchandise.js
+++ pages/api/buymerchandise.js
@@ -1,15 +1,43 @@
 import connectToMongo from '@/middleware/middleware';
-import MerchandiseForm from '@/models/MerchandiseForm'
+import MerchandiseForm from '@/models/MerchandiseForm';
+import nodemailer from 'nodemailer';
 
 const handler = async (req, res) => {
     try {
         const updatedFormData = req.body;
+
         // Save form data to MongoDB
         await MerchandiseForm.create(updatedFormData);
+
+        // Send confirmation email
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.MAIL_USER,
+                pass: process.env.MAIL_PASS,
+            },
+        });
+
+        const mailOptions = {
+            from: process.env.MAIL_USER,
+            to: updatedFormData.email, // assuming your form has an 'email' field
+            subject: 'Confirmation of Purchase Registration for Techikka Sub-Council Merchandise',
+            text: `We are delighted to inform you that your recent purchase of merchandise from the Technical Sub-Council has been successfully registered. Thank you for choosing our products. We truly appreciate your support and are committed to providing you with high-quality products.\n\nWe will verify your payment and reach out to you within 48 hours to deliver your merchandise to you. ✨✨\n\nBest regards,\nTechnical Sub-Council\nHBTU, Kanpur`,
+        };
+
+        await transporter.sendMail(mailOptions);
+
         res.status(200).json({ success: true, message: 'Ordered successfully' });
     } catch (error) {
         console.error('Error submitting form:', error.message);
-        res.status(500).json({ success: false, error: 'Internal Server Error' });
+
+        // Check if the error is related to MongoDB
+        if (error.name === 'MongoError') {
+            res.status(500).json({ success: false, error: 'Error saving to the database' });
+        } else {
+            // Handle other errors (e.g., email sending)
+            res.status(500).json({ success: false, error: 'Internal Server Error' });
+        }
     }
 };
 
--- pages/api/submiteventform.js
+++ pages/api/submiteventform.js
@@ -1,17 +1,44 @@
 import connectToMongo from '@/middleware/middleware';
 import EventForm from '@/models/EventForm';
+import nodemailer from 'nodemailer';
 
 const handler = async (req, res) => {
     try {
         const updatedFormData = req.body;
-console.log(updatedFormData)
+        console.log(updatedFormData);
+
         // Save form data to MongoDB
         await EventForm.create(updatedFormData);
+
+        // Send confirmation email
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.MAIL_USER,
+                pass: process.env.MAIL_PASS,
+            },
+        });
+
+        const mailOptions = {
+            from: process.env.MAIL_USER,
+            to: updatedFormData.email, // assuming your form has an 'email' field
+            subject: 'Successful Registration of Technical Sub-Council Event',
+            text: `We are thrilled to inform you that your registration for the upcoming event of Technical Sub-Council has been successfully completed. Your presence at the event is highly appreciated, and we are confident that it will be an enriching experience for you.\n\nIf you have any queries or require further information regarding the event, feel free to contact us.\n\nWe look forward to welcoming you at the event and fostering meaningful connections within our Technical community.\n\nBest Regards,\nTechnical Sub-Council`,
+        };
+
+        await transporter.sendMail(mailOptions);
 
         res.status(200).json({ success: true, message: 'Form submitted successfully' });
     } catch (error) {
         console.error('Error submitting form:', error.message);
-        res.status(500).json({ success: false, error: 'Internal Server Error' });
+
+        // Check if the error is related to MongoDB
+        if (error.name === 'MongoError') {
+            res.status(500).json({ success: false, error: 'Error saving to the database' });
+        } else {
+            // Handle other errors (e.g., email sending)
+            res.status(500).json({ success: false, error: 'Internal Server Error' });
+        }
     }
 };
 
